e3a24043dcbbccf6b1e7dca3683711f442d70476
carlinhos255 <perezcarlossantino@gmail.com>
2024-07-09 15:18:35 -0300

    logica1

---
+++ script.js
@@ -0,0 +1,32 @@
+var albumes = [
+    {
+      "artista": "The Beatles",
+      "nombre": "Let It Be",
+      "portada": "https://i.scdn.co/image/ab67616d0000b27384243a01af3c77b56fe01ab1",
+      "genero": "Pop-Rock"
+    },
+    {
+        "artista": "The Beatles",
+        "nombre": "Let It Be 2",
+        "portada": "https://i.scdn.co/image/ab67616d0000b27384243a01af3c77b56fe01ab1",
+        "genero": "Pop-Rock"
+      } 
+];
+
+
+function random(){
+
+    //generamos un numero aleatorio
+    numero = Math.floor((Math.random()*100)%3);
+
+    //selecionamos el album
+    album = albumes[numero];
+
+    //establecemos titulo
+    document.getElementById("titulo").textContent = album["artista"] + " - " + album["nombre"];
+
+    //establecemos portada
+    document.getElementById("portada").src = album["portada"];
+}
+
+document.getElementById("random").addEventListener("click", random)
